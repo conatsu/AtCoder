d2d1d0d8da9ac39eaad9f161118ec1dd0e0f66cb
TakuroMomose <conatsu0923@TakuroMomosenoMac-mini.local>
2023-10-27 17:38:56 +0900

    add TESSOKU a08.rb

---
+++ TESSOKU/a08.rb
@@ -0,0 +1,30 @@
+H, W = gets.chomp.split.map(&:to_i)
+Xtable = Array.new(H) { gets.chomp.split.map(&:to_i) }
+Q = gets.chomp.to_i
+queries = Array.new(Q) { gets.chomp.split.map(&:to_i) }
+
+# 各行の累積和を計算
+sums_row = Array.new(H) { Array.new(W, 0) }
+H.times do |i|
+    W.times do |j|
+        if j == 0
+            sums_row[i][j] = Xtable[i][j]
+        else
+            sums_row[i][j] = sums_row[i][j - 1] + Xtable[i][j]
+        end
+    end
+end
+
+# クエリの答えを計算
+queries.each do |query|
+    h1, w1, h2, w2 = query
+    sum = 0
+    (h1 - 1).upto(h2 - 1) do |i|
+        if w1 == 1
+            sum += sums_row[i][w2 - 1]
+        else
+            sum += sums_row[i][w2 - 1] - sums_row[i][w1 - 2]
+        end
+    end
+    puts sum
+end
