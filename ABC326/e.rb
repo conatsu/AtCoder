24ffed83b5df735cb9d9160441a1bc393bd7434b
TakuroMomose <conatsu0923@TakuroMomosenoMac-mini.local>
2023-10-29 19:00:29 +0900

    modify e.rb

--- ABC326/e.rb
+++ ABC326/e.rb
@@ -1,30 +1,16 @@
 # frozen_string_literal: true
 
-MOD = 998_244_353
+mod = 998_244_353
+n = gets.to_i
+a = gets.split.map(&:to_i)
 
-N = gets.to_i
-A = gets.split.map(&:to_i)
+p = n.pow(mod - 2, mod)
+q = 1 + n.pow(mod - 2, mod)
+res = 0
 
-def inverse(n, mod = MOD)
-  pow(n, mod - 2)
+a.each do |v|
+  res = (res + p * v) % mod
+  p = (p * q) % mod
 end
 
-def pow(a, b, mod = MOD)
-  result = 1
-  while b.positive?
-    result = result * a % mod if b & 1 == 1
-    a = a * a % mod
-    b >>= 1
-  end
-  result
-end
-
-total = 0
-N.times do |x|
-  N.times do |j|
-    total += (N - x) * A[j] % MOD
-    total %= MOD
-  end
-end
-
-puts total * inverse(N) % MOD * inverse(N) % MOD
+puts res
